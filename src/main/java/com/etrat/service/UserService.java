0256716cf1cddb2d6634373fe4f7acc7e10bc40c
arsham <arsham.sedaghatbin@gmail.com>
2020-06-11 13:13:55 +0430

    change default password

--- src/main/java/com/etrat/service/UserService.java
+++ src/main/java/com/etrat/service/UserService.java
@@ -153,7 +153,7 @@ public class UserService {
         } else {
             user.setLangKey(userDTO.getLangKey());
         }
-        String encryptedPassword = passwordEncoder.encode("1234");
+        String encryptedPassword = passwordEncoder.encode("123456");
         user.setPassword(encryptedPassword);
         user.setResetKey(RandomUtil.generateResetKey());
         user.setResetDate(Instant.now());
