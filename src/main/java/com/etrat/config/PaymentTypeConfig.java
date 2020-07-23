4bd096cc1cee483cd27b70ac45e8bb5862735045
arsham <arsham.sedaghatbin@gmail.com>
2020-07-23 17:27:17 +0430

    add delicate

--- src/main/java/com/etrat/config/PaymentTypeConfig.java
+++ src/main/java/com/etrat/config/PaymentTypeConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+//@Component
 public class PaymentTypeConfig {
     @Autowired
     private RestTemplate restTemplate;
