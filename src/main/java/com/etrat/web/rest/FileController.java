58a77c218fbe62ddaeae300d37be8b4f90de0292
arshamsedaghatbin <arsham.sedaghatbin@gmail.com>
2021-01-21 14:50:27 +0330

    add version controller

--- src/main/java/com/etrat/web/rest/FileController.java
+++ src/main/java/com/etrat/web/rest/FileController.java
@@ -2,12 +2,14 @@ package com.etrat.web.rest;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,11 +22,13 @@ public class FileController {
     ResourceLoader resourceLoader;
 
     @RequestMapping(path = "/{name}", method = RequestMethod.GET)
-    public ResponseEntity<Resource> download(@PathVariable String name) throws IOException {
+    public ResponseEntity<byte[]> download(@PathVariable String name) throws IOException {
         // ...
 
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("image/" + name);
+
         //        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
-        Resource resource1 = resourceLoader.getResource("classpath:/image/" + name);
-        return ResponseEntity.ok().contentLength(resource1.getFile().length()).contentType(MediaType.IMAGE_PNG).body(resource1);
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(StreamUtils.copyToByteArray(inputStream));
     }
 }
